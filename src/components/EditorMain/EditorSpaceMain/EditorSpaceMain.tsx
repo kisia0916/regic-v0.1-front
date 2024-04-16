3144fcc136af4b3c27e9780661cecbf1de32f577
kisia0916 <fumiharuabe@gmail.com>
2024-04-16 23:28:40 +0900

    editor update

--- src/components/EditorMain/EditorSpaceMain/EditorSpaceMain.tsx
+++ src/components/EditorMain/EditorSpaceMain/EditorSpaceMain.tsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./EditorSpaceMain.css"
 
 function EditorSpaceMain(){
-    const [codeLines,setCodeLines] = useState<string[]>(["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a",])
+    const lineHeight:number = 21
+    const [codeLines,setCodeLines] = useState<string[]>(["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a",])
+    const [lineNum,setLineNum] = useState<number>(codeLines.length)
+    useEffect(()=>{
+        setLineNum(codeLines.length)
+    },[codeLines])
     return(
         <div className="EditorSpaceMain">
-            <div className="EditorSpaceMainLeft">
-                {codeLines.map((i,index)=>{
-                    return (
-                        <div className="EditorSpaceLineNumberWarp">
-                        <span className="EditorSpaceLineNumber">{index+1}</span>     
-                        </div>
-                    )
-                })}          
+            <div className="EditorSpaceMainLeftWarp">
+                <div className="EditorSpaceMainLeft">
+                    {codeLines.map((i,index)=>{
+                        return (
+                            <div className="EditorSpaceLineNumberWarp">
+                            <span className="EditorSpaceLineNumber">{index+1}</span>     
+                            </div>
+                        )
+                    })}          
+                </div>
             </div>
             <div className="EditorSpaceMainRight">
-
+                    <textarea style={{height:`${lineHeight*lineNum}px`}} className="EditorSpaceMainEditArea"></textarea>
             </div>
         </div>
     )   
